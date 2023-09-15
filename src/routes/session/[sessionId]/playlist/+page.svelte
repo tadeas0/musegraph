291997c0690fc2fef5d247fb95dc3ed99c58be0e
Tadeas Pala <tadapala@seznam.cz>
2023-09-15 15:13:40 +0200

    update playlist creation form to skeleton ui

--- src/routes/session/[sessionId]/playlist/+page.svelte
+++ src/routes/session/[sessionId]/playlist/+page.svelte
@@ -3,15 +3,16 @@
     import type { ArtistSimilar } from "$lib/types/ArtistSimilar";
     import { getContext } from "svelte";
     import type { SessionStore } from "$lib/stores/SessionStore";
-    import { toast } from "$lib/notification";
     import { createPlaylist } from "$lib/api";
     import { goto } from "$app/navigation";
     import { spotifyUserStore } from "$lib/stores/SpotifyProfileStore";
     import LoadingOverlay from "$lib/components/LoadingOverlay.svelte";
+    import FaSpotify from "svelte-icons/fa/FaSpotify.svelte";
 
     const sessionStore: SessionStore = getContext(SESSION_CONTEXT_KEY);
     let selectedArtists: string[] = [];
     let loading = false;
+    let error: string | null = null;
 
     function filterDuplicateArtists(artistStack: ArtistSimilar[]) {
         return artistStack.filter(
@@ -34,9 +35,9 @@
             const artists = formData.getAll("artists").map((d) => d.toString());
             const name = formData.get("name");
             if (name === null || name.toString() === "") {
-                toast("Missing playlist name", "error");
+                error = "Missing playlist name";
             } else if (artists.length === 0) {
-                toast("No artists selected", "error");
+                error = "No artists selected";
             } else if ($spotifyUserStore) {
                 const playlist = await createPlaylist(
                     name.toString(),
@@ -45,13 +46,13 @@
                 );
                 goto(`./playlist/created?${new URLSearchParams({ ...playlist })}`);
             } else {
-                toast("User not logged in", "error");
+                error = "User not logged in";
             }
         } catch (err) {
             if (err instanceof Error) {
-                toast(err.message, "error");
+                error = err.message;
             } else {
-                toast("Something went wrong", "error");
+                error = "Something went wrong";
                 console.error(err);
             }
         } finally {
@@ -65,57 +66,70 @@
 </script>
 
 <form
-    class="flex w-full flex-col items-center gap-8 lg:w-4/6"
+    class="mt-4 flex flex-col items-start gap-8"
     on:submit|preventDefault={handleSubmit}
 >
-    {#if loading}
-        <LoadingOverlay spinnerPosition="top" />
-    {/if}
-    <div class="ml-auto flex w-36 flex-col items-stretch">
-        <button class="rounded-md bg-blue-400 p-2 text-white" type="submit">
-            Create playlist
-        </button>
+    <div class="w-full">
+        {#if error}
+            <aside class="variant-ghost-error alert">
+                {error}
+            </aside>
+        {/if}
     </div>
-    <input
-        name="name"
-        class="self-start rounded-md border-2 border-blue-400 p-2"
-        placeholder="Playlist name"
-    />
-    <fieldset class="flex w-full flex-col items-start gap-4 border-t-2">
-        <div class="my-4 flex items-start space-x-2">
-            <input
-                class="mr-4"
-                type="checkbox"
-                checked={areAllSelected}
-                id="selectAll"
-                on:click={handleSelectAll}
-            />
-            <div class="grid gap-1.5 leading-none">
-                <label for="selectAll" class="text-xl font-medium leading-none">
-                    Select all
-                </label>
-                <p class="text-sm text-slate-600">{selectedArtists.length} selected</p>
-            </div>
+    {#if loading}
+        <div class="fixed bottom-0 left-0 right-0 top-0 z-20">
+            <LoadingOverlay />
         </div>
+    {/if}
+    <button class="btn variant-filled-primary self-end">
+        <span class="w-6"><FaSpotify /></span>
+        <span>Create playlist</span>
+    </button>
+    <label class="label">
+        <span>Playlist name</span>
+        <input
+            class="input"
+            title="Playlist name"
+            type="text"
+            name="name"
+            placeholder="My playlist name"
+        />
+    </label>
+    <div class="flex items-start space-x-2">
+        <input
+            class="checkbox"
+            type="checkbox"
+            checked={areAllSelected}
+            id="selectAll"
+            on:click={handleSelectAll}
+        />
+        <div class="grid gap-1.5 leading-none">
+            <label for="selectAll" class="text-xl font-medium leading-none">
+                Select all
+            </label>
+            <p class="text-sm">{selectedArtists.length} selected</p>
+        </div>
+    </div>
+    <div class="space-y-2 pl-8">
         {#each artistList as a}
-            <div class="flex items-start space-x-2 pl-4">
+            <div class="items-top flex space-x-2">
                 <input
-                    class="mr-4"
+                    class="checkbox"
                     type="checkbox"
                     name="artists"
-                    value={a.artist.name}
                     id={a.artist.name}
+                    value={a.artist.name}
                     bind:group={selectedArtists}
                 />
                 <div class="grid gap-1.5 leading-none">
-                    <label for={a.artist.name} class="text-lg font-medium leading-none">
+                    <label class="label" for={a.artist.name}>
                         {a.artist.name}
                     </label>
-                    <p class="text-sm text-slate-600">
-                        {a.artist.genres.join(", ")}
+                    <p class="text-sm text-tertiary-500">
+                        {a.artist.genres.slice(0, 2).join(", ")}
                     </p>
                 </div>
             </div>
         {/each}
-    </fieldset>
+    </div>
 </form>
--- src/routes/session/[sessionId]/playlist/created/+page.svelte
+++ src/routes/session/[sessionId]/playlist/created/+page.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
     import type { SpotifyPlaylist } from "$lib/types/SpotifyPlaylist";
     import { page } from "$app/stores";
+    import FaSpotify from "svelte-icons/fa/FaSpotify.svelte";
 
     export let data: SpotifyPlaylist;
 </script>
 
-<div class="flex w-full flex-col items-center lg:w-4/6">
-    <h1 class="mt-4 text-xl">
+<div class="flex w-full flex-col items-center">
+    <h1 class="h1 mt-4">
         Created playlist <span class="font-bold">{data.name}</span>
     </h1>
-    <a target="_blank" href={data.url} class="mt-6 rounded-md bg-blue-400 p-2 text-white">
-        Show on spotify
+    <a target="_blank" href={data.url} class="btn variant-filled-primary mt-8">
+        <span class="w-6"><FaSpotify /></span>
+        <span>Show on spotify</span>
     </a>
     <a
         href={`/session/${$page.params.sessionId}/discover/graph`}
-        class="mt-6 rounded-md bg-red-500 p-2 text-white"
+        class="btn variant-filled-error mt-6"
     >
         Back to graph
     </a>
