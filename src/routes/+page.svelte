88c8e441354414adad5cd1380e335ca26b73198a
Tadeas Pala <tadapala@seznam.cz>
2023-09-15 16:12:33 +0200

    make landing page more responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
     import MdArrowForward from "svelte-icons/md/MdArrowForward.svelte";
 </script>
 
-<div class="mt-8 flex flex-col items-start justify-start p-2 md:flex-row">
-    <div class="relative flex flex-col items-center md:items-start lg:px-64">
+<div class="mt-8 flex flex-col items-start justify-start p-2 lg:flex-row">
+    <div class="relative flex flex-col items-center md:items-start xl:pl-64">
         <a href="/" class="gradient-heading h1 mb-4 font-bold">MuseGraph</a>
         <h2 class="h2 mb-2 max-w-lg text-center md:text-left">
             The music recommendation and visualization system
